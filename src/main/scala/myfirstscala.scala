f51be5081c96b163771d97979436fd049c11ad35
Chan Yung Hann <135926126+markuscyh@users.noreply.github.com>
2024-11-16 22:59:29 +0800

    links that may be useful
    they are in the myfirstscala.scala file

--- src/main/scala/myfirstscala.scala
+++ src/main/scala/myfirstscala.scala
@@ -3,6 +3,8 @@ import scalafx.application.JFXApp3.PrimaryStage
 import java.io.FileReader
 
 val etc: os.Path = os.root / "etc"
+//https://www.includehelp.com/scala/string-split-method-with-example.aspx
+//https://docs.scala-lang.org/toolkit/os-read-directory.html
 
 object MyApp extends JFXApp3:
 
