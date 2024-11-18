d1506e13e0e7c29cb72b4692b21fd94ef8a58119
Chan Yung Hann <135926126+markuscyh@users.noreply.github.com>
2024-11-18 18:41:05 +0800

    File reading attempt 2
    Code now puts information into list

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -2,18 +2,17 @@ import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 import scala.io.Source
 
-object MyApp extends JFXApp3:
 
-  val filename = "hospital.csv"
+@main def main(): Unit =
+  val filename = "./src/main/resources/hospital.csv"
 
   // code retrieved from https://stackoverflow.com/questions/31453511/how-to-read-a-text-file-using-relative-path-in-scala
-  for (line <- Source.fromResource(filename).getLines) {
-    println(line)
-  }
+  val bufferedSource = Source.fromFile(filename)
+  var data: List[List[String]] = List()
 
-  
+  for (line <- bufferedSource.getLines().drop(1))
+    val column = line.split(",").map(_.trim).toList
+    data = data :+ column
 
-  override def start(): Unit =
-    stage = new PrimaryStage()
-
-end MyApp
+  bufferedSource.close()
+  println(data)
