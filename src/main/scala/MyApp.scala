6a6509ce50663073a38f39b9fbd571a4768af3e0
Yoong Kok Keong <kevinyoongkokkeong@gmail.com>
2024-11-23 16:26:08 +0800

    Testing highest bed
    Testing the code for the highest amount of bed among states with 2 states (Johor and Selangor)

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,18 +1,60 @@
-import scalafx.application.JFXApp3
-import scalafx.application.JFXApp3.PrimaryStage
 import scala.io.Source
 
+abstract class State(val date: String, val state: String, val beds: Int, val bedsCovid: Int, val bedsNoncrit: Int, val admittedPui: Int, val admittedCovid: Int, val admittedTotal: Int, val dischargedPui: Int, val dischargedCovid: Int, val dischargedTotal: Int, val hospCovid: Int, val hospPui: Int, val hospNoncovid: Int) {
+  def displayInfo(): Unit
+}
 
-@main def main(): Unit =
-  val filename = "./src/main/resources/hospital.csv"
+class Johor(_date: String,
+            _beds: Int,
+            _bedsCovid: Int,
+            _bedsNoncrit: Int,
+            _admittedPui: Int,
+            _admittedCovid: Int,
+            _admittedTotal: Int,
+            _dischargedPui: Int,
+            _dischargedCovid: Int,
+            _dischargedTotal: Int,
+            _hospCovid: Int,
+            _hospPui: Int,
+            _hospNoncovid: Int) extends State(_date, "Johor", _beds, _bedsCovid, _bedsNoncrit, _admittedPui, _admittedCovid, _admittedTotal, _dischargedPui, _dischargedCovid, _dischargedTotal, _hospCovid, _hospPui, _hospNoncovid) {
+  override def displayInfo(): Unit = {
+    println(s"Date: $date, State: Johor, Beds: $beds, Beds Covid: $bedsCovid, Beds Non-Critical: $bedsNoncrit, Admitted PUI: $admittedPui, Admitted Covid: $admittedCovid, Admitted Total: $admittedTotal, Discharged PUI: $dischargedPui, Discharged Covid: $dischargedCovid, Discharged Total: $dischargedTotal, Hospitalized Covid: $hospCovid, Hospitalized PUI: $hospPui, Hospitalized Non-Covid: $hospNoncovid")
+  }
+}
 
-  // code retrieved from https://stackoverflow.com/questions/31453511/how-to-read-a-text-file-using-relative-path-in-scala
-  val bufferedSource = Source.fromFile(filename)
-  var data: List[List[String]] = List()
+class Selangor(_date: String,
+            _beds: Int,
+            _bedsCovid: Int,
+            _bedsNoncrit: Int,
+            _admittedPui: Int,
+            _admittedCovid: Int,
+            _admittedTotal: Int,
+            _dischargedPui: Int,
+            _dischargedCovid: Int,
+            _dischargedTotal: Int,
+            _hospCovid: Int,
+            _hospPui: Int,
+            _hospNoncovid: Int) extends State(_date, "Selangor", _beds, _bedsCovid, _bedsNoncrit, _admittedPui, _admittedCovid, _admittedTotal, _dischargedPui, _dischargedCovid, _dischargedTotal, _hospCovid, _hospPui, _hospNoncovid) {
+  override def displayInfo(): Unit = {
+    println(s"Date: $date, State: Selangor, Beds: $beds, Beds Covid: $bedsCovid, Beds Non-Critical: $bedsNoncrit, Admitted PUI: $admittedPui, Admitted Covid: $admittedCovid, Admitted Total: $admittedTotal, Discharged PUI: $dischargedPui, Discharged Covid: $dischargedCovid, Discharged Total: $dischargedTotal, Hospitalized Covid: $hospCovid, Hospitalized PUI: $hospPui, Hospitalized Non-Covid: $hospNoncovid")
+  }
+}
 
-  for (line <- bufferedSource.getLines().drop(1))
-    val column = line.split(",").map(_.trim).toList
-    data = data :+ column
+@main def main(): Unit = {
+  val filename = "hospital.csv"
 
-  bufferedSource.close()
-  println(data)
+  val source = Source.fromResource(filename)
+  val lines = source.getLines().drop(1) // Drop the header line
+
+  val states = lines.collect {
+    case line if line.contains("Johor") =>
+      val Array(date, state, beds, bedsCovid, bedsNoncrit, admittedPui, admittedCovid, admittedTotal, dischargedPui, dischargedCovid, dischargedTotal, hospCovid, hospPui, hospNoncovid) = line.split(",")
+      new Johor(date, beds.toInt, bedsCovid.toInt, bedsNoncrit.toInt, admittedPui.toInt, admittedCovid.toInt, admittedTotal.toInt, dischargedPui.toInt, dischargedCovid.toInt, dischargedTotal.toInt, hospCovid.toInt, hospPui.toInt, hospNoncovid.toInt)
+    case line if line.contains("Selangor") =>
+      val Array(date, state, beds, bedsCovid, bedsNoncrit, admittedPui, admittedCovid, admittedTotal, dischargedPui, dischargedCovid, dischargedTotal, hospCovid, hospPui, hospNoncovid) = line.split(",")
+      new Selangor(date, beds.toInt, bedsCovid.toInt, bedsNoncrit.toInt, admittedPui.toInt, admittedCovid.toInt, admittedTotal.toInt, dischargedPui.toInt, dischargedCovid.toInt, dischargedTotal.toInt, hospCovid.toInt, hospPui.toInt, hospNoncovid.toInt)
+  }.toList
+
+  val stateWithMostBeds = states.maxBy(_.beds)
+  stateWithMostBeds.displayInfo()
+}
